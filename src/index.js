83bf4739fed7d6e04c09fa94879870ea64e151fe
WaltVenditi <waltvenditti@gmail.com>
2021-12-21 11:30:23 -0500

    working on fetch functions

--- src/index.js
+++ src/index.js
@@ -1,51 +1,76 @@
 import "./style.css";
+const { format } = require('date-fns');
+const { zonedTimeToUtc } = require('date-fns-tz')
 
 // write a function that takes location and returns weather object or whatever
 
+const inputSearch = document.querySelector("#input-search");
+const btnSubmit = document.querySelector("#btn-submit");
 
-/*
-function getCurrentWeather(location) {
-    let wData;
-    fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=cda7acdfc26ad0b9968959a4af1eca7e`
-      )
-      .then(response => response.json())
-      .then(data => wData = data)
-      .then(() => console.log(wData));
-    return wData;
-}
-console.log(getCurrentWeather('kyiv'));
-*/
-
-
-
-function kToF(kTemp) {
-    return (((kTemp - 273.15)*1.8)+32.0).toFixed(1);
+function meterToFeet(meters) {
+  return (meters * 3.2808).toFixed(0);
 }
 
-async function getCurrentWeather(location) {
+function feetToMile(feet) {
+  return (feet / 5280).toFixed(1);
+}
+
+async function getCurrentWeather(location, units) {
   try {
     const re = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=cda7acdfc26ad0b9968959a4af1eca7e`
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&APPID=cda7acdfc26ad0b9968959a4af1eca7e`,
+      { mode: "cors" }
     );
     const wData = await re.json();
     return wData;
   } catch (error) {
     console.log(error);
-    return false; 
+    return false;
   }
 }
 
-async function unpackWeather() {
-    const data = await getCurrentWeather('kyiv');
-    console.log(`Weather for ${data.name}`);
-    console.log(kToF(data.main.temp));
-    console.log(data.weather[0].description);
-    console.log(`Local time is ${data}`);
-    let date = new Date();
-    console.log(date.getTime());
+async function getForecast(location, units) {
+  try {
+    const re = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=${units}&appid=cda7acdfc26ad0b9968959a4af1eca7e`,
+      { mode: "cors" }
+    );
+    const wData = await re.json();
+    console.log(wData);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
-unpackWeather();
+async function unpackWeather(location, units) {
+  const data = await getCurrentWeather(location, units);
+  console.log(data);
+  const wData = {
+    name: data.name,
+    units: `${units}`,
+    country: data.sys.country,
+    timezone: data.timezone,
+    temp: data.main.temp.toFixed(1),
+    feels_like: data.main.feels_like,
+    desc: data.weather[0].description,
+    humidity: data.main.humidity,
+    wind: data.wind.speed.toFixed(1),
+    vis: feetToMile(meterToFeet(data.visibility)),
+  };
+  return wData;
+}
 
+btnSubmit.addEventListener("click", async () => {
+  const location = inputSearch.value;
+  const wData = await unpackWeather(location, 'imperial');
+  console.log(wData);
+});
 
+//getForecast("kyiv", 'imperial');
+let date = new Date()
+let fDate = format(date, 'MM-dd-yyyy hh:mmaa')
+let utcDate = zonedTimeToUtc(date);
+console.log(date);
+console.log(fDate);
+console.log(utcDate);
